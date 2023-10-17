6c744716bf55673f4746491ce6552d32fd4e491d
Matteo-Detalle <144224004+Matteo-Detalle@users.noreply.github.com>
2023-10-17 12:02:41 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+﻿import pygame
+pygame.init()
+pygame.display.set_caption("Anti-virus Game")
+screen = pygame.display.set_mode((1080,720))
+
+running = True
+background = pygame.image.load(r"assets\background.png")
+
+while running:
+
+    screen.blit(background, (0,0))
+    pygame.display.flip()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+
